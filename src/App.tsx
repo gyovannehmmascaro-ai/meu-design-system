09d8713e86fb5cb7f6f91e57ff23e67dabcc23d2
Giovanne Mascaro <gyovannehmmascaro@gmail.com>
2026-02-27 20:14:36 -0300

    primeiro commit - projeto criado

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Meu Design System 🎨
+      </h1>
+      <p className="mt-2 text-gray-500">
+        Tailwind funcionando!
+      </p>
+    </div>
+  )
+}
+
+export default App
